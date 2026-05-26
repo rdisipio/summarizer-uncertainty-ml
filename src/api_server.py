cbef66c5352b65fd1839ae556ace1bcf4330cd3d
Riccardo Di Sipio <riccardo.disipio@gmail.com>
2026-05-25 21:41:55 -0400

    wip

--- src/api_server.py
+++ src/api_server.py
@@ -322,14 +322,6 @@ def _build_default_service() -> ScoringService:
         base_model = AutoModelForSeq2SeqLM.from_pretrained(base_model_name)
         peft_model = PeftModel.from_pretrained(base_model, adapter_path, is_trainable=True)
         tokenizer = AutoTokenizer.from_pretrained(base_model_name)
-
-        # Apply 8-bit dynamic quantization after LoRA adapter is loaded
-        logger.info("Applying 8-bit dynamic quantization to LoRA model")
-        peft_model = torch.quantization.quantize_dynamic(
-            peft_model,
-            {torch.nn.Linear},
-            dtype=torch.qint8
-        )
         backend = LoraLaplaceBackend(peft_model=peft_model, tokenizer=tokenizer, device=device)
         sampler = load_laplace_sampler(sampler_path)
         return SummaryUncertaintyScorer(backend=backend, posterior_sampler=sampler)
